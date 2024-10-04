ef398092e18f10ef2940c2344aedd15ed61b7a2b
Chloe <zzoizisoo@gmail.com>
2024-10-04 17:17:46 +0900

    debounce

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -184,28 +184,6 @@ export async function fetchFilteredInvoices(
 				}
 			}
 		])
-
-	// 	const invoices = await sql<InvoicesTable>`
-    //   SELECT
-    //     invoices.id,
-    //     invoices.amount,
-    //     invoices.date,
-    //     invoices.status,
-    //     customers.name,
-    //     customers.email,
-    //     customers.image_url
-    //   FROM invoices
-    //   JOIN customers ON invoices.customer_id = customers.id
-    //   WHERE
-    //     customers.name ILIKE ${`%${query}%`} OR
-    //     customers.email ILIKE ${`%${query}%`} OR
-    //     invoices.amount::text ILIKE ${`%${query}%`} OR
-    //     invoices.date::text ILIKE ${`%${query}%`} OR
-    //     invoices.status ILIKE ${`%${query}%`}
-    //   ORDER BY invoices.date DESC
-    //   LIMIT ${ITEMS_PER_PAGE} OFFSET ${offset}
-    // `;
-
 		return invoices.toArray();
 	} catch (error) {
 		console.error("Database Error:", error);
